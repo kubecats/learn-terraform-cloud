141926f969021da051bc90b3668614c57b385be3
lhs <lhs.shcard@gmail.com>
2023-09-23 18:57:39 +0900

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,19 @@
-terraform {
+# terraform {
 
-  cloud {
-    organization = "hs-cloud"
+#   cloud {
+#     organization = "hs-cloud"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+#     workspaces {
+#       name = "learn-terraform-cloud"
+#     }
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      # version = "~> 3.28.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       # version = "~> 3.28.0"
+#     }
+#   }
 
-  # required_version = ">= 0.14.0"
-}
+#   # required_version = ">= 0.14.0"
+# }
